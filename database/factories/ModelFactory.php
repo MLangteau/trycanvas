466f914a64923801e88f6b5d4b2f5d370cb87233
MLangteau <langteauM@gmail.com>
2017-07-18 12:42:05 -0400

    added event-list.blade and view for upcoming and past events

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,6 +12,8 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\User;
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
@@ -20,5 +22,21 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(\App\Modules\Event\Event::class, function (Faker\Generator $faker) {
+    $start_date = \Carbon\Carbon::now()->addDays($faker->randomElement([1,2,3,4,5,6,7,8,9]));
+    $end_date = $start_date->copy()->addDays($faker->randomElement([1,2,3,4,5,6,7,8,9]));
+
+    return [
+        'title' => $faker->sentence(5),
+        'description' => $faker->paragraph(5),
+        'address' => $faker->address,
+        'lat' => $faker->latitude,
+        'long' => $faker->longitude,
+        'start_date' => $start_date->format('Y-m-d'),
+        'end_date' => $start_date->format('Y-m-d'),
+        'user_id' => factory(User::class)->create()->id,
     ];
 });
